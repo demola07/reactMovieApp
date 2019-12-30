ea7dda32314664928fc39d1f90cc28534ab3516a
demola07 <45898468+demola07@users.noreply.github.com>
2019-12-30 17:58:16 +0100

    styled header component with styled components

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {
+  StyledHeader,
+  StyledRMDBLogo,
+  StyledTMDBLogo
+} from '../styles/StyledHeader';
+
+import RMDBLogo from '../images/reactMovie_logo.png';
+import TMDBLogo from '../images/tmdb_logo.svg';
+
 const Header = props => {
-  return <div>Header</div>;
+  return (
+    <StyledHeader>
+      <div className='header-content'>
+        <StyledRMDBLogo src={RMDBLogo} alt='rmdb-logo' />
+        <StyledTMDBLogo src={TMDBLogo} alt='tmdb-logo' />
+      </div>
+    </StyledHeader>
+  );
 };
 
 Header.propTypes = {};
