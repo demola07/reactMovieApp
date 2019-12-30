1ab04849d6749107228c5e5dbc662eccdf94b00e
demola07 <45898468+demola07@users.noreply.github.com>
2019-12-31 00:16:39 +0100

    worked on getting movie data from the movie api to render on movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,7 +7,12 @@ import Actor from './elements/Actor';
 import Grid from './elements/Grid';
 import Spinner from './elements/Spinner';
 
+import { useMovieFetch } from './hooks/useMovieFetch';
+
 const Movie = ({ movieId }) => {
+  const [movie, loading, error] = useMovieFetch(movieId);
+  console.log(movie);
+
   return (
     <>
       <Navigation></Navigation>
