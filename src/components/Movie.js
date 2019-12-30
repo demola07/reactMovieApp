739e65baaf435f94c0e31469fe5b9cd7d45c5abd
demola07 <45898468+demola07@users.noreply.github.com>
2019-12-30 23:41:20 +0100

    created scaffolding for movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import Navigation from './elements/Navigation';
+import MovieInfo from './elements/MovieInfo';
+import MovieInfoBar from './elements/MovieInfoBar';
+import Actor from './elements/Actor';
+import Grid from './elements/Grid';
+import Spinner from './elements/Spinner';
+
 const Movie = ({ movieId }) => {
-  return <div>Movie: {movieId}</div>;
+  return (
+    <>
+      <Navigation></Navigation>
+      <MovieInfo></MovieInfo>
+      <MovieInfoBar></MovieInfoBar>
+      <Grid>
+        <Actor></Actor>
+      </Grid>
+      <Spinner></Spinner>
+    </>
+  );
 };
 
 export default Movie;
